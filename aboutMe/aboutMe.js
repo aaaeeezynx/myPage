74fa38a54f6a43b537e617331e352ff266492e50
aaaeeezynx <aaaeee33@gmail.com>
2023-12-08 09:56:37 +0800

    fix aboutme page

---
+++ aboutMe/aboutMe.js
@@ -0,0 +1,10 @@
+// var imgbox=document.querySelector(URL("./Close X.ico"))
+
+// document.move=function(e){
+//     var x=e.clientX
+//     var y=e.clientY
+    
+//     imgbox.style.top=y+5+'px'
+//     imgbox.style.left=x+5+'px'
+//     imgbox.style.display="block"
+// }
